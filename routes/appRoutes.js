847946e1e94c63af23305e05ffb0a9128dec55a2
KIRUBAHARAN8878M <kirubaharan8878m@gmail.com>
2024-08-17 14:21:25 +0530

    initial commit

---
+++ routes/appRoutes.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import multer from 'multer';
+
+import  { createApp, deleteApp, getAllApps, getSpecificApp, updateApp, launchApp} from '../controllers/appsControllers.js';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
+// Create
+router.post('/create', upload.single('icon'),createApp);
+
+// Read
+router.get('/get', getAllApps);
+
+// Update
+router.put('/update/:id', upload.single('icon'), updateApp);
+
+// Delete
+router.delete('/delete/:id', deleteApp);
+
+//get specific details of the app using ID
+router.get('/get/:id', getSpecificApp); 
+
+router.post('/launcher/:id', launchApp);
+
+export default router;
